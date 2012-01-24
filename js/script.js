cd69f2ce1ee611ab8a7b1beb45670b81632e63c2
Maurycy Damian Wasilewski <damian.wasilewski@gmail.com>
2012-01-24 23:16:53 +0100

    prepared to be offline

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@
 
 */
 
+$(function(){
+    if(navigator.onLine) {
+        // tutaj dodaj jakąś synchronizację bazy z serwerem
+        log('Rozpoczynanie synchronizacji z serverem...   FAIL (not implemented)');
+    }
 
+    
+});
 
 
 
